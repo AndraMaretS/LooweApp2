f99548f3e7784024354b6c99de3798e94cc9a323
Andra Maret <andramaret4@gmail.com>
2016-11-14 20:36:00 +0700

    Layout cake

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl404132231/loowe2/BirthdayActivity.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xirpl404132231.loowe2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BirthdayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birthday);
+
+        setTitle("Birthday Cake");
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl404132231/loowe2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl404132231/loowe2/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl404132231.loowe2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, KueActivity.class));
+            }
+        });
     }
 }
