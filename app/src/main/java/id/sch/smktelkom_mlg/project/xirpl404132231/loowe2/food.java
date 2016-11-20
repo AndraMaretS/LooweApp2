dbe58ec81e6b99f51b83e0d1da7798a202e4bb6c
Andra Maret <andramaret4@gmail.com>
2016-11-20 17:25:12 +0700

    connect tradisional

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl404132231/loowe2/food.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl404132231/loowe2/food.java
@@ -13,18 +13,28 @@ import android.widget.Button;
  * Created by Erron on 11/19/2016.
  */
 
-public class food extends Fragment implements View.OnClickListener {
+public class food extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.food, container, false);
-        Button cake = (Button) v.findViewById(R.id.button);
-        cake.setOnClickListener(this);
-        return v;
-    }
 
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(getActivity().getApplication(), KueActivity.class);
-        startActivity(intent);
+        Button cake = (Button) v.findViewById(R.id.button);
+        cake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), KueActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button tes = (Button) v.findViewById(R.id.button2);
+        tes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), TraditionalActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
