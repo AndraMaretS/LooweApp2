133abd7d3393a893d3f368492a633ff8889b98ce
Andra Maret <andramaret4@gmail.com>
2016-11-20 09:27:01 +0700

    Connect cake

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl404132231/loowe2/food.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl404132231/loowe2/food.java
@@ -1,22 +1,30 @@
 package id.sch.smktelkom_mlg.project.xirpl404132231.loowe2;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Erron on 11/19/2016.
  */
 
-public class food extends Fragment {
-    @Nullable
+public class food extends Fragment implements View.OnClickListener {
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.food, container, false);
+        Button cake = (Button) v.findViewById(R.id.button);
+        cake.setOnClickListener(this);
         return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity().getApplication(), KueActivity.class);
+        startActivity(intent);
     }
 }
